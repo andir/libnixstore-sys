349f16f72ab49a8178287a7d2155fdd61605841e
Andreas Rammhold <andreas@rammhold.de>
2019-05-05 01:47:08 +0200

    only build the bindings on opt-in, it is a major pain during a carnix build

--- build.rs
+++ build.rs
@@ -3,24 +3,47 @@ extern crate pkg_config;
 
 use std::env;
 use std::path::PathBuf;
+use std::io::{self, Write};
 
-fn main() {
-    println!("cargo:rustc-link-lib=nixstore-c");
 
-    let include_paths = pkg_config::probe_library("nixstore-c").unwrap().include_paths;
-    for path in include_paths.iter() {
-        println!("cargo:include={}", path.to_string_lossy());
-    }
+fn generate_bindings(p: PathBuf) {
+    let lib = pkg_config::probe_library("nixstore-c").unwrap();
+    println!("lib: {:?}", lib);
+    let builder = lib.include_paths.iter().filter_map(|p| {
+                let mut p = p.clone();
+                p.push("nixstore-c.h");
+                let stderr = io::stderr();
+                let mut handle = stderr.lock();
+                let s = format!("path: {:?}", p);
+                handle.write(&s.into_bytes());
+                if p.exists() {
+                    Some(p)
+                } else {
+                    None
+                }
+    }).fold(bindgen::Builder::default(), |b, p| b.header(p.to_string_lossy()));
 
-//    let builder = include_paths.iter().fold(bindgen::Builder::default(), |b, p| b.header(p.to_string_lossy() + "/nixstore-c.h"));
-
-    let bindings = bindgen::Builder::default()
+    let bindings = builder
         .header("wrapper.h")
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
+        .write_to_file(p)
         .expect("Couldn't write bindings!");
+}
+
+fn main() {
+
+    println!("cargo:rustc-link-lib=nixstore-c");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_path = out_path.join("bindings.rs");
+
+
+    if env::var("GENERATE_BINDINGS").is_ok() {
+        generate_bindings(out_path);
+    } else {
+        std::fs::copy("bindings/bindgen_bindings.rs", out_path).expect("Failed to copy");
+    }
 }
