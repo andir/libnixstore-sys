331160655af8f5fc7d571a2a3bbb8e73df211d44
Andreas Rammhold <andreas@rammhold.de>
2019-05-05 13:58:11 +0200

    bump libnixstore-c revision

--- shell.nix
+++ shell.nix
@@ -3,8 +3,8 @@ let
   libnixstore-c = callPackage (fetchFromGitHub {
     owner = "andir";
     repo = "libnixstore-c";
-    rev = "2ce29c26a4bca55bb5b83fa97f045659675bcfa7";
-    sha256 = "1jk798ck2ppkkw9i1p07rqk0xsi9bv89a62idwmzgxw6lniipby0";
+    rev = "16031bdfef9164f528918c1be8fd5ccb39da1fa1";
+    sha256 = "0y99ybdpghknrq6vn4m0zd67768rbrvl4fd6k9xkvkzzn9cqsjv9";
   }) {};
 in mkShell {
   buildInputs = [ libnixstore-c llvmPackages.clang-unwrapped.lib ];
